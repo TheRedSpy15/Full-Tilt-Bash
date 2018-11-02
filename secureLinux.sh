aecd97df5f8540e9a380ada43213e2c2bfa61d18
TheRedSpy15 <hjadar15@protonmail.com>
2018-11-02 17:39:44 -0400

    disable root login ssh

--- secureLinux.sh
+++ secureLinux.sh
@@ -8,3 +8,12 @@ apt-get install clamav
 
 echo "Installing anti-rootkit"
 sudo apt-get install rkhunter
+
+File="/etc/ssh/sshd_config"
+if grep -q STRING_YOU_ARE_CHECKING_FOR "$File"; 
+then
+echo "Disabling root login ssh"
+echo "DenyUsers root" >> /etc/ssh/sshd_config
+else
+echo "Root login for ssh already disabled"
+fi

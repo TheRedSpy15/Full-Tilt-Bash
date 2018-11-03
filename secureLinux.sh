b23c4101bf3785200ab7d50c36d3cfa714083108
TheRedSpy15 <hjadar15@protonmail.com>
2018-11-02 21:11:16 -0400

    disable insecure IO, install fail2ban

--- secureLinux.sh
+++ secureLinux.sh
@@ -18,10 +18,10 @@ sudo apt-get install rkhunter
 File="/etc/ssh/sshd_config"
 if grep -q STRING_YOU_ARE_CHECKING_FOR "$File"; 
 then
-echo "Disabling root login ssh"
-echo "DenyUsers root" >> /etc/ssh/sshd_config
+    echo "Disabling root login ssh"
+    echo "DenyUsers root" >> /etc/ssh/sshd_config
 else
-echo "Root login for ssh already disabled"
+    echo "Root login for ssh already disabled"
 fi
 
 echo "Removing insecure protocols"
@@ -29,3 +29,10 @@ yum erase xinetd ypserv tftp-server telnet-server rsh-server
 
 echo "Enforcing maximum password age"
 chage -M 100 root
+
+echo "Disable insecure IO ports"
+echo "blacklist firewire-core" >> /etc/modprobe.d/firewire.conf
+echo "blacklist thunderbolt" >> /etc/modprobe.d/thunderbolt.conf
+
+echo "installing fail2ban"
+apt-get install fail2ban

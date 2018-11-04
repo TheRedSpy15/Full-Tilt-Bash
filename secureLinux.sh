18b34453c277b9c69c14942176f9986adb66b76d
TheRedSpy15 <hjadar15@protonmail.com>
2018-11-04 09:46:14 -0500

    added comments

--- secureLinux.sh
+++ secureLinux.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-## TODO: add comments to document
 ## TODO: finish blacklisted domains
 ## TODO: remove untrustworthy ca certificates
 
@@ -10,9 +9,11 @@ then
     exit
 fi
 
+## Firwall - need if statement
 echo "Enabling firewall"
 ufw enable
 
+## Clamav
 echo "Checking for clamav"
 if [ $(dpkg-query -W -f='${Status}' clamav 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
@@ -22,6 +23,7 @@ else
     echo "Clamav already installed"
 fi
 
+## rkhunter
 echo "Checking for rkhunter"
 if [ $(dpkg-query -W -f='${Status}' rkhunter 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
@@ -31,6 +33,7 @@ else
     echo "rkhunter already installed"
 fi
 
+## Root login
 echo "Checking if root login allowed"
 File="/etc/ssh/sshd_config"
 if grep -q 'DenyUsers root' "$File"; 
@@ -41,14 +44,17 @@ else
     echo "Root login already disabled"
 fi
 
-sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' "$FIle"
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' "$FIle" ## is a check within itself
 
+## Insecure protocols - need if statement
 echo "Removing insecure protocols"
 yum erase xinetd ypserv tftp-server telnet-server rsh-server
 
+## Maximum password aga - no need for if statement
 echo "Enforcing maximum password age (100 days)"
 chage -M 100 root
 
+## Insecure IO - thunderbolt
 echo "Disabling insecure IO ports"
 File="/etc/modprobe.d/thunderbolt.conf"
 if [ -e "$File" ]; 
@@ -60,6 +66,7 @@ then
     fi
 fi
 
+## Insecure IO - firewire
 File="/etc/modprobe.d/firewire.conf"
 if [ -e "$File" ]; 
 then
@@ -70,6 +77,7 @@ then
     fi
 fi
 
+## fail2ban
 echo "Checking for fail2ban"
 if [ $(dpkg-query -W -f='${Status}' fail2ban 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
@@ -79,6 +87,7 @@ else
     echo "fail2ban already installed"
 fi
 
+## Malicious domains
 echo "Black-listing malicious domains"
 File="/etc/hosts"
 if ! grep -q 'totalvirus.com' "$File"; 
